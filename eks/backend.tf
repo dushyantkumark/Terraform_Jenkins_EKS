06a03aac60b3fdcaa6d078106493e17cb703a231
Dushyant Kumar <116090315+dushyantkumark@users.noreply.github.com>
2024-05-14 22:45:43 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-file-april-072024"
+    bucket = "terraform-state-file-14-05-2024"
     key    = "eks/terraform.tfsate" /* where you store your tf state file */
     region = "ap-south-1"
   }
